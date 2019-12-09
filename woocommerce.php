8abfaa91a72b8a9a8571bb7b017cc8ae94cdbf6a
Tim Hughes <timbocode@gmail.com>
2019-12-09 19:16:41 +0000

    Add dummy woocommerce.php

---
+++ woocommerce.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WooCommerce Placeholder
+Description: Makes WooCommerce extensions think WooCommerce is present and active.
+Author: ZigPress
+Version: 9999.0
+Requires at least: 4.9
+Tested up to: 5.3
+Author URI: https://www.zigpress.com/
+*/
