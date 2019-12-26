2e7326907b1592edaae1559ca4620d88cadc209e
Laurence Bahiirwa <laurence@mediauganda.com>
2019-12-26 18:13:38 +0300

    Proof of concept

--- cc-compat.php
+++ cc-compat.php
@@ -11,39 +11,37 @@
  * Contributors: SAThemba, ZigPress
  */
 
+if( ! function_exists( 'wp_get_current_user' ) ) {
+    require( ABSPATH . "wp-includes/pluggable.php") ; 
+}
+    
+// Require because it is not always loaded.
+require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+global $wp_filesystem;
+  
 $dir = "woocommerce";
 $file_to_copy = 'woocommerce.txt';
 $cc_compat_woo_folder = dirname( __FILE__ );
 
-if( ! function_exists( 'wp_get_current_user' ) ) {
-    require( ABSPATH . "wp-includes/pluggable.php") ; 
-}
-  
 // if user can install plugins, then create access.
 if ( current_user_can( 'install_plugins' ) ) {
-    
+
     // Make a folder called woocommerce
-    if( is_dir($dir) === false ) {
-        mkdir($dir);
+    if( $wp_filesystem->is_dir($dir) === false ) {
+        $wp_filesystem->mkdir($dir);
+        // copy woocommerce.txt into .php in woocommerce folder
+        $wp_filesystem->move( $dir . $file_to_copy, $dir . 'woocommerce.php');
+        
+        // activate it
+        activate_plugins( 'woocommerce/woocommerce.php' );
+        
+        // deactivate cc-compat-woo
+        deactivate_plugins( 'cc-compat-woo/cc-compat-woo.php' );
+
+        // delete cc-compat-woo
+        $wp_filesystem->delete('cc-compat-woo/cc-compat-woo.php');
+
+        $wp_filesystem->rmdir( $cc_compat_woo_folder );
     }
-
-    // copy woocommerce.txt to woocommerce folder
-    copy( $file_to_copy, $dir );
-
-    // Rename woocommerce.txt to woocommerce.php
-    rename( $dir . 'woocommerce.txt', $dir . 'woocommerce.php');
-
-    // Require because it is not always loaded.
-    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-
-    // activate it
-    activate_plugins( 'woocommerce/woocommerce.php' );
-    
-    // deactivate cc-compat-woo
-    deactivate_plugins( 'cc-compat-woo/cc-compat-woo.php' );
-
-    // delete cc-compat-woo
-    unlink('cc-compat-woo/cc-compat-woo.php');
-
-    rmdir( $cc_compat_woo_folder );
 }
