33e9249ac04bb2390ff67425b62385dc34662876
timbocode <timbocode@gmail.com>
2020-12-14 13:23:47 +0000

    Bump version number and create change log

--- woocommerce.php
+++ woocommerce.php
@@ -3,11 +3,11 @@
  * Plugin Name: CC Compatibility for Woo Addons
  * Description: A compatibility plugin for some WooCommerce addons to work with Classic Commerce.
  * Author: Classic Commerce Research Team
- * Version: 9999.0
+ * Version: 9999.1
  * Requires at least: 1.0.0
- * Tested up to: 1.1.2
+ * Tested up to: 1.2.0
  * Author URI: https://www.classiccommerce.cc/
- * 
+ *
  * Contributors: SAThemba, ZigPress
  */
 
@@ -16,7 +16,7 @@ defined( 'ABSPATH' ) || exit;
 // Load the Update Client to manage updates for the CC Compatibility for Woo Addons plugin
 include_once dirname( __FILE__ ) . '/includes/UpdateClient.class.php';
 
-define( 'CCWOOADDONSCOMPAT_VERSION', '9999.0' ); // DO NOT change the version in the plugin header or the Earth will fall on you :P 
+define( 'CCWOOADDONSCOMPAT_VERSION', '9999.1' );  // Make sure the version number (and in the headers) is higher then current Woo version
 
 define( 'CCWOOADDONSCOMPAT__FILE__', __FILE__ );
 define( 'CCWOOADDONSCOMPAT_PATH', plugin_dir_path( CCWOOADDONSCOMPAT__FILE__ ) );
@@ -27,7 +27,7 @@ if( !defined( 'CCWOOADDONSCOMPAT_PLUGIN_BASE' ) ) {
 
 function ccwooaddonscompat_hide_view_details( $plugin_meta, $plugin_file, $plugin_data, $status ) {
 	if( CCWOOADDONSCOMPAT_PLUGIN_BASE == $plugin_file ) {
-		unset( $plugin_meta[2] );		
+		unset( $plugin_meta[2] );
 	}
 	return $plugin_meta;
 }
